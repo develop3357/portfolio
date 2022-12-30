07db12f512bacdd02ace9a7022dc7499dea08d30
Jihyeon Kim <develop3357@gmail.com>
2022-12-30 22:58:51 +0900

    feat: add my tech stacks page

---
+++ src/routes/TechStacks.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import TechStacksCardHolder from "../components/TechStacksCardHolder";
+import tech from "../models/TechModel";
+
+const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+function TechStacks() {
+  return (
+    <Article>
+      {Object.keys(tech).map((category) => (
+        <TechStacksCardHolder
+          key={category}
+          category={category}
+          techStacks={tech[category]}
+        />
+      ))}
+    </Article>
+  );
+}
+
+export default TechStacks;
