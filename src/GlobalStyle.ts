e861a88052e24d5f60283b23eac5c1fc918e9aea
Jihyeon Kim <develop3357@gmail.com>
2023-01-02 22:22:59 +0900

    change global styles
    font, anchor tag color

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,20 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url(https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/fira_code.min.css);
 
   * {
     box-sizing: border-box;
   }
   body {
     font-weight: 300;
-    font-family: 'Fira Code', sans-serif;
+    font-family: sans-serif;
     background-color: ${(props) => props.theme.Background};
     color: ${(props) => props.theme.WhiteBlackColor};
     line-height: 1.2;
   }
   a {
     text-decoration: none;
-    color: ${(props) => props.theme.LinksColor};
+    color: inherit;
   }
 `;
